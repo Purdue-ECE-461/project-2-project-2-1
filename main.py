2cdd08e90cd62a5756048a833a2799c1a852cce1
David Li <davidli2881@gmail.com>
2021-11-15 04:13:09 +0000

    added endpoint to resource

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
-    api.add_resource(CreatePackage, '/package')
+    api.add_resource(CreatePackage, '/package', endpoint='/package')
     app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python3_render_template]
 # [END gae_python38_render_template]
