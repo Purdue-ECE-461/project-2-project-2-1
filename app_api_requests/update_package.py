de4b2ba24c4f1ebeebe855f9b35c73eca332f1ad
Nia Isom <nisom@purdue.edu>
2021-11-18 04:16:05 +0000

    Draft of UpdatePackage path

---
+++ app_api_requests/update_package.py
@@ -0,0 +1,97 @@
+from google.cloud import datastore
+
+from flask_restful import Resource
+from flask import request
+
+import json
+
+class UpdatePackage(Resource): # also why is this a POST request
+    def put(self, id): # pass in URL parameters
+        request.get_data() # Get everything from the request
+
+        auth_header = request.headers.get('X-Authorization')
+        if auth_header is None:
+            return {}, 400
+        # TODO: add authorization here in the future
+        
+        # Get the inputted "id" from the URL path
+        # input_id = request.args.get("id")
+        input_id = request.args['id']
+        
+        # Get data from the request body
+        decoded_data = request.data.decode("utf-8") # Decode body of the data
+        request_body = json.loads(decoded_data)
+        
+        try:
+            new_package_name = request_body['metadata']['Name']
+            new_package_version = request_body['metadata']['Version']
+            new_package_id = request_body['metadata']['ID'] # should be the same ID
+
+            new_package_content = request_body['data']['Content']
+            new_package_url = request_body['data']['URL']
+            new_package_js_program = request_body['data']['JSProgram']
+
+        except Exception:
+            return {}, 400
+
+        # Check that the ID in the path MATCHES the ID in the request body
+        if (input_id != new_package_id):
+            response = {
+                "description": "Malformed request (e.g. no such package)."
+                "Message": f"ID from path: {input_id}. Does not match ID in request body {new_package_id}."
+            }
+            return response, 400 # should this be "jsonify{}"
+
+        # Check that the Name, Version, and ID --> Produce a Package
+        datastore_client = datastore.Client()
+
+        query = datastore_client.query(kind='package')
+        query.add_filter("Name", "=", new_package_name)
+        query.add_filter("Version", "=", new_package_version)
+        query.add_filter("ID", "=", input_id) # same as new_package_id
+        results = list(query.fetch())
+
+        # Check to see if this Name+Version+ID combo actually does exists in the registry
+        if (len(results) == 0 ): # This Combo doesn't exist
+            response = {
+                "description": "Malformed request (e.g. no such package)."
+                "Message": f"No package with  Name+Version+ID combo given in the request body."
+            }
+            return response, 400
+        
+
+        # If we get here: Update the package (create a new one to replace)
+        # Get the package/entity
+        key = datastore_client.key('package', input_id)
+        package_entity = datastore.Entity(key, exclude_from_indexes=["Content"])
+        
+        # Update properties: https://cloud.google.com/datastore/docs/concepts/entities#properties_and_value_types
+        package_entity.update(
+            {
+                "Name" = new_package_name
+                "Version" = new_package_version
+                "ID" = input_id
+                "Content" = new_package_content
+                "URL" = new_package_url
+                "JSProgram" = new_package_js_program
+                # Should we be re-computing these before uploading to the Datastore ...?
+                # Whenever we upload, we should compute the scores then ?
+                "RampUp" = 0    # package_entity["RampUp"]
+                "Correctness" = 0    # package_entity["Correctness"]
+                "BusFactor" = 0    # package_entity["BusFactor"]
+                "ResponsiveMaintainer" = 0    # package_entity["ResponsiveMaintainer"]
+                "LicenseScore" = 0    # package_entity["LicenseScore"]
+                "GoodPinningPractice" = 0    # package_entity["GoodPinningPractice"]
+                "Events" = []
+            }
+        )
+
+        # Update entity in the registry
+        datastore_client.put(package_entity)
+
+        response = {
+            'success': True
+        }
+
+        # Return response body and code
+        return response, 200    # jsonify{response}
--- main.py
+++ main.py
@@ -26,6 +26,7 @@ from google.cloud import datastore
 # Internal imports
 # Import your desired 
 from app_api_requests.create_package import CreatePackage as CreatePackage
+from app_api_requests.update_package import UpdatePackage as UpdatePackage
 
 # Instantiates a client
 datastore_client = datastore.Client()
@@ -36,14 +37,13 @@ api = Api(app)
 ###### Loop for Uploading and Fetching below #######
 
 # [ UPLOADING ENTITIES/ITEMS TO DATASTORE ]
-# This store_time method:
-#  uses the Datastore client libraries to create a new "entity" in Datastore (upload == put!)
-# Datastore entities are:
-#   data objects that consist of keys and properties.
-# In this case, the entity's key is its custom kind, "visit".
-# The entity also has one property, timestamp, containing time of a page request.
-# entity = (key="visit", property1="timestamp", property2="", ...)
-
+    # This store_time method:
+    #  uses the Datastore client libraries to create a new "entity" in Datastore (upload == put!)
+    # Datastore entities are:
+    #   data objects that consist of keys and properties.
+    # In this case, the entity's key is its custom kind, "visit".
+    # The entity also has one property, timestamp, containing time of a page request.
+    # entity = (key="visit", property1="timestamp", property2="", ...)
 def store_time(dt):
     # setting the key
     entity = datastore.Entity(key=datastore_client.key('visit'))
@@ -56,10 +56,10 @@ def store_time(dt):
     datastore_client.put(entity) 
 
 # [ FETCHING ENTITIES/ITEMS FROM DATASTORE ]
-# This fetch_times method:
-# uses the key 'visit' to query the database >> "Get all the Entries/Uploads with teh key "visit".
-#  ( only get the 10 most recent "visit" entities)
-#  And then STORES those entities --(visit, time) pairings-- in a list (in descending order).
+    # This fetch_times method:
+    # uses the key 'visit' to query the database >> "Get all the Entries/Uploads with teh key "visit".
+    #  ( only get the 10 most recent "visit" entities)
+    #  And then STORES those entities --(visit, time) pairings-- in a list (in descending order).
 def fetch_times(limit):
     query = datastore_client.query(kind='visit')
     query.order = ['-timestamp'] # the "-" indicates descending order by the property "timestamp"
@@ -80,6 +80,7 @@ def root():
     return render_template('index.html', times=times)
 
 api.add_resource(CreatePackage, '/package', endpoint='/package')
+api.add_resource(UpdatePackage, '/package<string:id>', endpoint='/package<string:id>') # resource_class_args : args.params to be forwards to constructor
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
