01fbd7bf36c0674a18a426bcb6b944aea74cf749
Nia Isom <nisom@purdue.edu>
2021-11-24 06:14:56 +0000

    Working PUT, GET, and DELETE for /package/{id}

--- app_api_requests/package_by_id.py
+++ app_api_requests/package_by_id.py
@@ -8,17 +8,14 @@ import sys
  
  
 def print_to_stdout(*a):
- 
-    # Here a is the array holding the objects
-    # passed as the argument of the function
     print(*a, file = sys.stdout)
 
 
 # @api.resource('/package/<string:id>')
 class PackageById(Resource): # also why is this a POST request
-    def put(self, id): # pass in URL path parameters
-        # print_to_stdout("Request went through")
-        request.get_data() # Get everything from the request
+    def put(self, id): # UPDATE package
+        # print_to_stdout("PUT request went through")
+        request.get_data() # Get everything from the request/URL (path params)
 
         auth_header = request.headers.get('X-Authorization')
         if auth_header is None:
@@ -47,13 +44,13 @@ class PackageById(Resource): # also why is this a POST request
         except Exception:
             return {}, 400
 
-        # Check that the ID in the path MATCHES the ID in the request body
+        # Check that the ID in the PATH matches the ID in the request body
         if (input_id != new_package_id):
             response = {
-                "description": "Malformed request (e.g. no such package).",
-                "Message": "Inputted ID does not match ID in request body."
+                "description": "Malformed request",
+                "message": "Inputted ID (in URL) does not match ID in request body."
             }
-            return response, 400 # should this be "jsonify{}"
+            return response, 400
 
         # Check that the Name, Version, and ID --> Produce a Package
         datastore_client = datastore.Client()
@@ -68,34 +65,45 @@ class PackageById(Resource): # also why is this a POST request
         if (len(results) == 0 ): # This Combo doesn't exist
             response = {
                 "description": "Malformed request (e.g. no such package).",
-                "message": "Inputted ID does not match ID in request body."
+                "message": "Package Name or Version does not the match with the Package ID."
+            }
+            return response, 400
+        elif (len(results) >= 2 ): # Multiple matches. This should never happen.
+            response = {
+                "description": "Datastore Error.",
+                "message": "Multiple matches for the given ID in the datastore."
             }
             return response, 400
         
-
-        # If we get here: Update the package (create a new one to replace)
-        # Get the package/entity
+        # If we get here: Update the package
+        # Get/create the package/entity that we're going to update.
         key = datastore_client.key('package', input_id)
+        original_package = datastore_client.get(key)
         package_entity = datastore.Entity(key, exclude_from_indexes=["Content"])
+        # what does " exclude_from_indexes=["Content"] " do ?
         
         # Update properties: https://cloud.google.com/datastore/docs/concepts/entities#properties_and_value_types
         package_entity.update(
             {
-                "Name" : new_package_name,
-                "Version" : new_package_version,
-                "ID" : input_id,
+                "Name" : original_package["Name"],
+                "Version" : original_package["Version"],
+                "ID" : original_package["ID"],
+                
+                # According to the .yaml instructions, only the "PackageData" (3 fields above) are changing/being updated.
+                # All other fields (Name, Version, ID, Rating Scores, Event, etc) are the SAME
+                # Thus, only those 3 fields are are specified in this update
                 "Content" : new_package_content,
                 "URL" : new_package_url,
                 "JSProgram" : new_package_js_program,
-                # Should we be re-computing these before uploading to the Datastore ...?
-                # Whenever we upload, we should compute the scores then ?
-                "RampUp" : 0,    # package_entity["RampUp"]
-                "Correctness" : 0,    # package_entity["Correctness"]
-                "BusFactor" : 0,    # package_entity["BusFactor"]
-                "ResponsiveMaintainer" : 0 ,   # package_entity["ResponsiveMaintainer"]
-                "LicenseScore" : 0  ,  # package_entity["LicenseScore"]
-                "GoodPinningPractice" : 0 ,   # package_entity["GoodPinningPractice"]
-                "Events" : []
+                
+                # Copying over the original values
+                "RampUp" : original_package['RampUp'],
+                "Correctness" : original_package['Correctness'],
+                "BusFactor" : original_package['BusFactor'],
+                "ResponsiveMaintainer" : original_package['ResponsiveMaintainer'],
+                "LicenseScore" : original_package['LicenseScore'],
+                "GoodPinningPractice" : original_package['GoodPinningPractice'],
+                "Events" : original_package['Events']
             }
         )
 
@@ -107,14 +115,14 @@ class PackageById(Resource): # also why is this a POST request
         }
 
         # Return response body and code
-        return response, 200    # jsonify{response}
+        return 200    # jsonify{response}
 
-    def get(self, id):
-        print_to_stdout("GET Request went through")
+    def get(self, id): # DOWNLOAD package
+        # print_to_stdout("GET Request went through")
         # This request has NO request body to utilize
         # And DO NOT update the database
         # Simply GETS info based on the ID --> and Returns the Response JSON
-        request.get_data() # Get everything from the request
+        request.get_data() # Get everything from the request/URL (path params)
 
         auth_header = request.headers.get('X-Authorization')
         if auth_header is None:
@@ -124,61 +132,101 @@ class PackageById(Resource): # also why is this a POST request
         # Get the inputted "id" from the URL path
         input_id = request.view_args['id']
         
-        # Check that the Name, Version, and ID --> Produce a Package
+        # Check that ID --> Matches a Package
         datastore_client = datastore.Client()
-
         query = datastore_client.query(kind='package')
         query.add_filter("ID", "=", input_id) # same as new_package_id
         results = list(query.fetch())
 
+        # Check to see if the ID exists in the registry
+        if (len(results) == 0 ): # This ID doesn't exist
+            response = {
+                "code": -1,
+                "message": "An error occurred while retrieving package",
+                "description": "Inputted ID is not in the datastore."
+            }
+            return response, 500
+        elif (len(results) >= 2 ): # There are 2 IDs in the database. This should not happen ever.
+            response = {
+                "code": -1,
+                "message": "An error occurred while retrieving package",
+                "description": "The inputted ID (wrongfully) appears multiple times in the datastore."
+            }
+            return response, 500
+        
+        # If we get here: Return the package 
+        # Get the package/entity
+        key = datastore_client.key('package', input_id)
+        package_to_return = datastore_client.get(key)
+        
+        # Don't need to create an Entity, becuase we are not upserting/replacing anything in thge database
+        # Only getting a package
+        # package_entity = datastore.Entity(key, exclude_from_indexes=["Content"])
+        # what is " exclude_from_indexes=["Content"] " for ?
+
+        # try-catch here to be safe
+        try:
+            response = {
+                "metadata": {
+                    "Name": package_to_return["Name"],
+                    "Version": package_to_return["Version"],
+                    "ID": package_to_return["ID"]
+                },
+                "data": {
+                    "Content": package_to_return["Content"],
+                    "URL": package_to_return["URL"],
+                    "JSProgram": package_to_return["JSProgram"]
+                }
+            }
+        except Exception:
+            response = {
+                "code": -1,
+                "message": "An error occurred while retrieving package",
+                "description": "The specified ID has null/missing fields in the datastore"
+            }
+            return response, 500
+
+    
+        # Return response body and code
+        return response, 200
+
+    def delete(self, id): # DELETE PAckage
+        request.get_data() # Get everything from the request/URL (path params)
+
+        auth_header = request.headers.get('X-Authorization')
+        if auth_header is None:
+            return {}, 400
+        # TODO: add authorization here in the future
+        
+        # Get the inputted "id" from the URL path
+        input_id = request.view_args['id']
+        
+        # Check that ID --> Matches a Package
+        datastore_client = datastore.Client()
+        query = datastore_client.query(kind='package')
+        query.add_filter("ID", "=", input_id) # same as new_package_id
+        results = list(query.fetch())
 
         # Check to see if the ID exists in the registry
         if (len(results) == 0 ): # This ID doesn't exist
             response = {
-                "description": "Malformed request (e.g. no such ID in database).",
-                "message": "Inputted ID is not in the datastore."
+                "message": "No such package.",
             }
             return response, 400
         elif (len(results) >= 2 ): # There are 2 IDs in the database. This should not happen ever.
             response = {
-                "description": "Malformed request",
-                "message": "The inputted ID is seen more than once in the datastore."
+                "message": "The inputted ID (wrongfully) appears multiple times in the datastore."
             }
-            return response, 400
+            return response, 500
         
-
-        # If we get here: Return the package 
+        # If we get here: DELETE the package 
         # Get the package/entity
         key = datastore_client.key('package', input_id)
-        package_entity = datastore.Entity(key, exclude_from_indexes=["Content"])
-
+        datastore_client.delete(key)
         
-        package_to_return = datastore_client.get(key)
-
-        # I should do a try-catch here
         response = {
-            "metadata": {
-                "Name": package_to_return["Name"],
-                "Version": package_to_return["Version"],
-                "ID": package_to_return["ID"]
-            },
-            "data": {
-                "Content": package_to_return["Content"],
-                "URL": package_to_return["URL"],
-                "JSProgram": package_to_return["JSProgram"]
-            }
+            "message": "Package is deleted."
         }
 
-        # Return response body and code
-        
-        return response, 200
-
-
-    def delete(self, id):
-        response = {
-            'Success': "not implemented"
-        }
-
-        # Return response body and code
-        
-        return response, 200        
+        # Return no response body and the code
+        return 200        
