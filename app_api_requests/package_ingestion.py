cccab7bd38134e4eee0cb1e78008a664bc10114c
David Li <davidli2881@gmail.com>
2021-12-04 05:53:44 +0000

    added tests for auth, reset, and rate

--- app_api_requests/package_ingestion.py
+++ app_api_requests/package_ingestion.py
@@ -1,3 +1,4 @@
+from app_api_requests.datastore_client_factory import get_datastore_client
 from app_api_requests.package_rating.metrics import Metric
 import logging
 
@@ -6,7 +7,7 @@ from google.cloud import datastore
 def compute_package_scores(package_url):
     log = logging.getLogger()
     
-    datastore_client = datastore.Client()
+    datastore_client = get_datastore_client()
     query = datastore_client.query(kind='tokens')
     results = list(query.fetch())
     github_token = results[0]['GITHUB_TOKEN']
