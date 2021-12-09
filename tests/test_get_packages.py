4c42502880536ae268b8c99b3d53328a2cd789d2
Nick Schuch <schuch@purdue.edu>
2021-12-08 19:12:07 -0500

    Added a lot of test material to get through. This relies heavily upon Package Creation working on the local datastore emulator in order to work...

--- tests/test_get_packages.py
+++ tests/test_get_packages.py
@@ -8,46 +8,184 @@ def test_getpackages_empty_registry():
     client = get_datastore_client()
     clear_registry()
     
+    # Make sure an empty registry responds to requests correctly
+
     header = generate_header()
     query = [{
         "Name": '*' 
     }]
+    # Start with full registry request, with no offset    
+    response = requests.post('http://127.0.0.1:8080/packages', headers=header, json=query)
+    assert response.status_code == 200
+    # Same request, except for an offset in URL below
+    response = requests.post('http://127.0.0.1:8080/packages?offset=10', headers=header, json=query)
+    assert response.status_code == 200
+    
+    # Now request partial registry
+    query = [{
+        "Version": "1.2.3",
+        "Name": "Underscore"
+    },
+    {
+        "Version": "1.2.3-2.1.0",
+        "Name": "Lodash"
+    }]
+    response = requests.post('http://127.0.0.1:8080/packages', headers=header, json=query)
+    assert response.status_code == 200
+    # Same request, except for an offset in URL below
+    response = requests.post('http://127.0.0.1:8080/packages?offset=10', headers=header, json=query)
+    assert response.status_code == 200    
+    
+def test_getpackages_full_registry():
+    client = get_datastore_client()
+    clear_registry()
+    # First populate the registry with some packages (they only need Name/Version/ID)
+    header = generate_header()
+    
+    # Add Underscore
+    query = {
+    "metadata": {
+            "Name": "Underscore",
+            "Version": "1.0.1",
+            "ID": "underscore_local"
+    },
+    "data": {
+            "Content": "",
+            "JSProgram": "if (process.argv.length === 7) {\nconsole.log('\''Success'\'')\nprocess.exit(0)\n} else {\nconsole.log('\''Failed'\'')\nprocess.exit(1)\n}\n"
+    }
+    }
+    response = requests.post('http://127.0.0.1:8080/package', headers=header, json=query)
+    assert response.status_code == 201
+    
+    # Add Lodash
+    query = {
+    "metadata": {
+            "Name": "Lodash",
+            "Version": "1.2.5",
+            "ID": "lodash_local"
+    },
+    "data": {
+            "Content": "",
+            "JSProgram": "if (process.argv.length === 7) {\nconsole.log('\''Success'\'')\nprocess.exit(0)\n} else {\nconsole.log('\''Failed'\'')\nprocess.exit(1)\n}\n"
+    }
+    }
+    response = requests.post('http://127.0.0.1:8080/package', headers=header, json=query)
+    assert response.status_code == 201
+    
+    # Add netdata
+    query = {
+    "metadata": {
+            "Name": "Netdata",
+            "Version": "1.0.0",
+            "ID": "netdata"
+    },
+    "data": {
+            "URL": "https://github.com/netdata/netdata",
+            "JSProgram": "if (process.argv.length === 7) {\nconsole.log('\''Success'\'')\nprocess.exit(0)\n} else {\nconsole.log('\''Failed'\'')\nprocess.exit(1)\n}\n"
+    }
+    }
+    response = requests.post('http://127.0.0.1:8080/package', headers=header, json=query)
+    assert response.status_code == 201
+    
+    # Add debug
+    query = {
+    "metadata": {
+            "Name": "Debug",
+            "Version": "4.3.3",
+            "ID": "debug"
+    },
+    "data": {
+            "URL": "https://github.com/debug-js/debug",
+            "JSProgram": "if (process.argv.length === 7) {\nconsole.log('\''Success'\'')\nprocess.exit(0)\n} else {\nconsole.log('\''Failed'\'')\nprocess.exit(1)\n}\n"
+    }
+    }
+    response = requests.post('http://127.0.0.1:8080/package', headers=header, json=query)
+    assert response.status_code == 201
+    
+    # Now try to use get_packages to query the datastore
+    #header = generate_header()
+    query = [{
+            "Name": "*"
+            }]
 
     response = requests.post('http://127.0.0.1:8080/packages', headers=header, json=query)
     assert response.status_code == 200
-    print(response.content)
-    
-"""
-def test_getpackages_full_registry():
+
+def test_getpackages_partial_registry():
+    client = get_datastore_client()
+    clear_registry()
+    # First populate the registry with some packages (they only need Name/Version/ID)
     header = generate_header()
+    
+    # Add Underscore
     query = {
-        "metadata": {
-            "Name": "FreeCodeCamp",
-            "Version": "1.0.0",
-            "ID": "freeCodeCamp"
-        },
-        "data": {
+    "metadata": {
+            "Name": "Underscore",
+            "Version": "1.0.1",
+            "ID": "underscore_local"
+    },
+    "data": {
             "Content": "",
             "JSProgram": "if (process.argv.length === 7) {\nconsole.log('\''Success'\'')\nprocess.exit(0)\n} else {\nconsole.log('\''Failed'\'')\nprocess.exit(1)\n}\n"
-        }
     }
-
+    }
     response = requests.post('http://127.0.0.1:8080/package', headers=header, json=query)
     assert response.status_code == 201
-
-curl -i --location --request POST 'http://127.0.0.1:8080/package' \
---header 'X-Authorization: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' \
---data-raw '{
-	"metadata": {
-		"Name": "Lodash",
-		"Version": "1.2.5",
-		"ID": "lodash_local"
-	},
-	"data": {
-		"Content": "",
-		"JSProgram": "if (process.argv.length === 7) {\nconsole.log('\''Success'\'')\nprocess.exit(0)\n} else {\nconsole.log('\''Failed'\'')\nprocess.exit(1)\n}\n"
-	}
-}'
-def test_getpackages_partial_registry():
-    pass
-"""
+    
+    # Add Lodash
+    query = {
+    "metadata": {
+            "Name": "Lodash",
+            "Version": "1.2.5",
+            "ID": "lodash_local"
+    },
+    "data": {
+            "Content": "",
+            "JSProgram": "if (process.argv.length === 7) {\nconsole.log('\''Success'\'')\nprocess.exit(0)\n} else {\nconsole.log('\''Failed'\'')\nprocess.exit(1)\n}\n"
+    }
+    }
+    response = requests.post('http://127.0.0.1:8080/package', headers=header, json=query)
+    assert response.status_code == 201
+    
+    # Add netdata
+    query = {
+    "metadata": {
+            "Name": "Netdata",
+            "Version": "1.0.0",
+            "ID": "netdata"
+    },
+    "data": {
+            "URL": "https://github.com/netdata/netdata",
+            "JSProgram": "if (process.argv.length === 7) {\nconsole.log('\''Success'\'')\nprocess.exit(0)\n} else {\nconsole.log('\''Failed'\'')\nprocess.exit(1)\n}\n"
+    }
+    }
+    response = requests.post('http://127.0.0.1:8080/package', headers=header, json=query)
+    assert response.status_code == 201
+    
+    # Add debug
+    query = {
+    "metadata": {
+            "Name": "Debug",
+            "Version": "4.3.3",
+            "ID": "debug"
+    },
+    "data": {
+            "URL": "https://github.com/debug-js/debug",
+            "JSProgram": "if (process.argv.length === 7) {\nconsole.log('\''Success'\'')\nprocess.exit(0)\n} else {\nconsole.log('\''Failed'\'')\nprocess.exit(1)\n}\n"
+    }
+    }
+    response = requests.post('http://127.0.0.1:8080/package', headers=header, json=query)
+    assert response.status_code == 201        
+    
+    # Now try to use get_packages to query the datastore
+    #header = generate_header()
+    query = [{
+        "Version": "1.2.3",
+        "Name": "Underscore"
+    },
+    {
+        "Version": "1.2.3-2.1.0",
+        "Name": "Lodash"
+    }]
+    response = requests.post('http://127.0.0.1:8080/packages', headers=header, json=query)
+    assert response.status_code == 200
